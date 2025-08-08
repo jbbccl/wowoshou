4b69a4ff9773ad3cd710459c7498c2e4188cc091
lccbbj <lccbbjnoexisth1O3Iw6l@qq.com>
2025-08-09 05:40:05 +0800

    删除调试内容

--- src/player.cpp
+++ src/player.cpp
@@ -1,5 +1,3 @@
-
-
 #include "player.hpp"
 
 
@@ -7,25 +5,13 @@ void Player::_ready() {
     _animated_sprite = get_node<godot::AnimatedSprite>("AnimatedSprite");
     _collision_shape_head = get_node<godot::CollisionShape2D>("CollisionShape2DHead");
     _collision_shape_body = get_node<godot::CollisionShape2D>("CollisionShape2DBody");
-    _input = godot::Input::get_singleton();//此处返回空指针，移动到_process了
-    _screen_size = get_viewport_rect().size;//此处返回0,0，移动到_process了
-    
-    //TODO：
-    //找到_ready中不能正确取值的n原因
-    // godot::Vector2 velocity(0, 0);
-    // emit_signal("debug_information", this, velocity);
+    _input = godot::Input::get_singleton();
+    _screen_size = get_viewport_rect().size;
 }
 
 
 void Player::_process(const double delta) {
     godot::Vector2 velocity(0, 0);
-
-    // if(_input==nullptr){//临时解决——ready不能初始化问题
-    //     _input = godot::Input::get_singleton();//移动到此处
-    //     _collision_shape_head = get_node<godot::CollisionShape2D>("CollisionShape2DHead");
-    //     _collision_shape_body = get_node<godot::CollisionShape2D>("CollisionShape2DBody");
-    //     _animated_sprite = get_node<godot::AnimatedSprite>("AnimatedSprite");
-    // }
 
 	velocity.x = _input->get_action_strength("move_right") - _input->get_action_strength("move_left");
 	velocity.y = _input->get_action_strength("move_down") - _input->get_action_strength("move_up");
@@ -34,7 +20,6 @@ void Player::_process(const double delta) {
 		velocity = velocity.normalized() * speed;
         godot::Vector2 position = get_position();
         position += velocity * (real_t)delta;
-        _screen_size = get_viewport_rect().size;//移动到此处
         position.x = godot::Math::clamp(position.x, (real_t)0.0, _screen_size.x);
         position.y = godot::Math::clamp(position.y, (real_t)0.0, _screen_size.y);
         set_position(position);
